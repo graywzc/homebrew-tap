c4c38214fdbad1c39615dac4db03256c02ffd4cc
Larry Wang <graywzc@gmail.com>
2026-05-05 11:29:26 -0700

    Update screen-snipper to 0.1.1

--- Formula/screen-snipper.rb
+++ Formula/screen-snipper.rb
@@ -1,8 +1,8 @@
 class ScreenSnipper < Formula
   desc "Tiny macOS screen-region recorder for GIFs and MP4 video"
   homepage "https://github.com/graywzc/screen-snipper"
-  url "https://github.com/graywzc/screen-snipper/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "866a62fd6dd9524e4dc7894f8aeca78ee421e88af5b9983103141d86b87af42f"
+  url "https://github.com/graywzc/screen-snipper/archive/refs/tags/v0.1.1.tar.gz"
+  sha256 "7c0d1347cf0daff6f8c9816ca2f52fcc4683b0dd156ce1df9ef86dbb3b8b9b29"
   license :cannot_represent
 
   depends_on macos: :sonoma
