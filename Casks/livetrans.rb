1b3912c728fd056fb461461fce08c00956365dd5
Zhichun Wang <2048439+graywzc@users.noreply.github.com>
2026-09-25 18:06:54 -0700

    Update livetrans to 0.1.26 (#49)
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- Casks/livetrans.rb
+++ Casks/livetrans.rb
@@ -1,6 +1,6 @@
 cask "livetrans" do
-  version "0.1.25"
-  sha256 "f6abfdf626fb5c7381cde2a386107bbcc486ffacf7da1c600ba05fec60ebe45e"
+  version "0.1.26"
+  sha256 "01e292712990c83ab9e44516013e970465b436df4f398934ac3059629feb81f8"
 
   url "https://github.com/graywzc/live-trans/releases/download/v#{version}/LiveTrans.zip"
   name "LiveTrans"
